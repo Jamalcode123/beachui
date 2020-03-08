f1d3ac414e11a1e52dcee72978d16843c3d1cd0c
Jamal <ridge100k@gmail.com>
2020-03-07 20:57:45 -0500

    initial commit

---
+++ src/components/RoomContainer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import RoomFilter from "./RoomFilter";
+import RoomList from "./RoomList";
+import { withRoomConsumer } from "../Context";
+import Loading from "./Loading";
+
+function RoomContainer({ context }) {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <div>
+      <RoomFilter rooms={rooms} />
+      <RoomList rooms={sortedRooms} />
+    </div>
+  );
+}
+
+export default withRoomConsumer(RoomContainer);
+
+// import React from "react";
+// import RoomFilter from "./RoomFilter";
+// import RoomList from "./RoomList";
+// import { RoomConsumer } from "../Contex";
+// import Loading from "./Loading";
+
+//function RoomContainer() {
+//  return (
+//    <RoomConsumer>
+//      {value => {
+//        const { loading, sortedRooms, rooms } = value;
+//        if (loading) {
+//          return <Loading />;
+//        }
+//        return (
+//          <div>
+//            Hello From Rooms Container
+//            <RoomFilter rooms={rooms} />
+//            <RoomList rooms={sortedRooms} />
+//          </div>
+//        );
+//      }}
+//    </RoomConsumer>
+//  );
+//}
+
+// export default RoomContainer;
